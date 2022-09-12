9b6130a0f18226d08a1dad548c92556a8fd051c0
xzhy324 <xzhy324@qq.com>
2022-09-12 17:50:32 +0800

    feat(logger):html&file version

--- cgiserver/http_server.py
+++ cgiserver/http_server.py
@@ -23,7 +23,7 @@ class HTTPServer:
         """Start the server, and whenever a new connection comes,
         create a session process to process the connection.
         """
-        self.logger.info("server starting...")
+        self.logger.info("server running...")
         server_socket = socket.socket()
         server_socket.bind((self.host, self.port))
         server_socket.listen(5)
@@ -47,15 +47,25 @@ class HTTPServer:
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         self.serve_forever()
 
+    def __del__(self)->None:
+        logging.info("server has been shutdown!")
 
     def _setup_logger(self):
         logger = logging.getLogger()  # get root logger
-        handler = logging.FileHandler("./cgiserver/log/log.log",mode='a+', encoding='utf-8', delay=False)
-        formatter = logging.Formatter(
-            '[%(asctime)s] - [%(levelname)s] %(message)s')  # 规定输出格式
-        handler.setFormatter(formatter)  # 将输出格式指定到handler上
-        logger.addHandler(handler)
         logger.setLevel(level=logging.INFO)
+
+        file_formatter = logging.Formatter(
+            '[%(asctime)s] - [%(levelname)s] %(message)s')
+        file_handler = logging.FileHandler("./cgiserver/log/basic_log.log",mode='a+', encoding='utf-8', delay=False)
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+
+        html_formatter = logging.Formatter(
+            '<p>[%(asctime)s] - [%(levelname)s] %(message)s</p>')
+        html_handler =  logging.FileHandler("./cgiserver/log/log.html",mode='a+', encoding='utf-8', delay=False)
+        html_handler.setFormatter(html_formatter)
+        logger.addHandler(html_handler)
+
         return logger
 
 
