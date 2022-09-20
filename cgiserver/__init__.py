b2868ab78b7c407908c7b9c3a2e79ffeeb920396
0x404 <871206929@qq.com>
2022-09-20 13:44:44 +0800

    feat: support specify log dir from command line

--- cgiserver/__init__.py
+++ cgiserver/__init__.py
@@ -1,16 +1,29 @@
 """CGI Server"""
+import os
 from argparse import ArgumentParser
 from .router import route
 from .http_server import HTTPServer
+from .logging import init_logger
 
 
-def run(host: str = "127.0.0.1", port: int = 8888, max_connection: int = 20):
+def run(
+    host: str = "127.0.0.1",
+    port: int = 8888,
+    max_connection: int = 20,
+    log_dir: str = None,
+):
     """wraped HTTPServer run.
 
     Args:
         host (str, optional): server host. Defaults to "127.0.0.1".
         port (int, optional): server port. Defaults to 5500.
+        max_connection (int, optional): maximum thread num in server thread pool. Defaults to 20.
+        log_dir (str, optional): directory of log file. Defaults to `pwd/logs`
     """
+    if log_dir is not None:
+        os.environ["CGISERVER_LOGDIR"] = log_dir
+    init_logger()
+
     server = HTTPServer(host, port, max_connection)
     server.serve_forever()
 
@@ -21,9 +34,10 @@ def crun():
     parser.add_argument("--host", type=str, default="127.0.0.1", help="server's host")
     parser.add_argument("--port", type=int, default=8888, help="server's port")
     parser.add_argument("-m", "--maxc", type=int, default=20, help="max connections")
+    parser.add_argument("-l", "--logdir", type=str, default=None, help="log dir")
 
     args = parser.parse_args()
-    run(host=args.host, port=args.port, max_connection=args.maxc)
+    run(host=args.host, port=args.port, max_connection=args.maxc, log_dir=args.logdir)
 
 
 __all__ = ["route", "run", "crun"]
--- cgiserver/logging/logger.py
+++ cgiserver/logging/logger.py
@@ -1,5 +1,35 @@
 """logger"""
+import os
+import sys
 import logging
+
+
+def init_logger():
+    """init root logger"""
+    log_dir = os.environ.get("CGISERVER_LOGDIR", "./logs")
+    html_log = os.path.join(log_dir, "server_log.html")
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir, exist_ok=True)
+
+    # set up root logger
+    logger = logging.getLogger()
+    logger.setLevel(level=logging.INFO)
+
+    general_formatter = logging.Formatter("[%(asctime)s] - [%(levelname)s] %(message)s")
+    html_formatter = logging.Formatter(
+        "<p>[%(asctime)s] - [%(levelname)s] %(message)s</p>"
+    )
+    html_handler = logging.FileHandler(
+        html_log, mode="w+", encoding="utf-8", delay=False
+    )
+    console_handler = logging.StreamHandler(sys.stdout)
+
+    html_handler.setFormatter(html_formatter)
+    console_handler.setFormatter(general_formatter)
+
+    logger.addHandler(html_handler)
+    logger.addHandler(console_handler)
 
 
 def get_logger(name: str = None):
