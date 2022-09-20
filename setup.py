41ab7ecb59ad071a85bd72dab53213da3b269cc1
0x404 <871206929@qq.com>
2022-09-20 14:12:03 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="cgi-server",
+    version="1.2.0",
+    author="zengqunhong, xuzheyuan",
+    author_email="zengqunhong@gmail.com",
+    description="multi thread cgi server using python3",
+    url="https://github.com/0x404/cgi-server",
+    
+    packages=find_packages(),
+    
+    python_requires=">=3.9",
+    
+)
