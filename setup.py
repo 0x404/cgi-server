7895d7c0fe6657bdf20cd1d0279c3b1ae047214b
0x404 <871206929@qq.com>
2022-10-03 00:24:03 +0800

    feat: install dependences and load version from file

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
+import os
 from setuptools import setup, find_packages
+
+
+def dependences():
+    package_dir = os.path.dirname(os.path.realpath(__file__))
+    requirements = os.path.join(package_dir, "requirements.txt")
+    package_dep = []
+    if os.path.isfile(requirements):
+        with open(requirements, mode="r") as file:
+            package_dep = file.read().splitlines()
+    return package_dep
+
+
+def version():
+    package_dir = os.path.dirname(os.path.realpath(__file__))
+    version_file = os.path.join(package_dir, "cgiserver", "__version__.py")
+    result = {}
+    with open(version_file, mode="r") as file:
+        exec(file.read(), result)
+    return result["__version__"]
 
 
 setup(
     name="cgi-server",
-    version="1.2.4",
-    author="zengqunhong, xuzheyuan",
+    version=version(),
+    author="zengqunhong; xuzheyuan; luxinbo;",
     author_email="zengqunhong@gmail.com",
     description="multi thread cgi server using python3",
     url="https://github.com/0x404/cgi-server",
     packages=find_packages(),
     python_requires=">=3.9",
+    install_requires=dependences(),
 )
